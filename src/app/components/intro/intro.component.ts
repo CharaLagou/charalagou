c6c2384e3fd24607300a37690a5aac402a713ce9
CharaLagou <lagou.chara@gmail.com>
2020-06-09 21:29:03 +0200

    Intro component

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  pronounce(word: string, lang: string) {
+    if ('speechSynthesis' in window) {
+      let utterance = new SpeechSynthesisUtterance();
+      utterance.text = word;
+      utterance.lang = lang;
+      window.speechSynthesis.speak(utterance);
+    }
   }
-
 }

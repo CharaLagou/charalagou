44455be419c0f64fe65ba9e74f0af712326a1cb2
CharaLagou <lagou.chara@gmail.com>
2020-06-10 17:05:34 +0200

    Contact and about components
    -contact component improvements
    -base for about component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  /* escape() {
+    // Cannot close window anymore for security reasons
+    window.close();
+  } */
 }
